239f9340e248b26cdab5690cfc422731d2501a46
sagarpakhrin <sagarlama826@gmail.com>
2021-06-25 22:47:09 +0545

    fat: validation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,7 +4,15 @@ const UserService = require('./UserService');
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
-  await UserService.save(req.body);
+  const user = req.body;
+  if (!user.username) {
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+    });
+  }
+  await UserService.save(user);
   return res.send({ message: 'User Created' });
 });
 
